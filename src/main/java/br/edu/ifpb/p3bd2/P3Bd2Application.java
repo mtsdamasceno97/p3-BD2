302930423068803d2cdbf2838638f7d3fe1a4f03
Diego Alves Rodrigues <diego.alves@logiquesistemas.com.br>
2019-11-26 12:18:18 -0300

    Estruturação inicial da aplicação em Spring Boot. Origem: Spring Initializer.

---
+++ src/main/java/br/edu/ifpb/p3bd2/P3Bd2Application.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.p3bd2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P3Bd2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P3Bd2Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifpb/p3bd2/P3Bd2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.p3bd2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P3Bd2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

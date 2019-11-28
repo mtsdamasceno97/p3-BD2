eaf598eab2bb14ff829f86d16e163eacaf178f56
diego.alves <diego.alves@logiquesistemas.com.br>
2019-11-27 22:23:56 -0300

    Alteração do tipo de retorno da stored procedure renavam() de char para varchar. Linkagem do método gerarRenavam com a stored procedure renavam().

--- src/main/java/br/edu/ifpb/p3bd2/repositorio/VeiculoRepositorio.java
+++ src/main/java/br/edu/ifpb/p3bd2/repositorio/VeiculoRepositorio.java
@@ -1,6 +1,16 @@
 package br.edu.ifpb.p3bd2.repositorio;
 
 import br.edu.ifpb.p3bd2.modelo.Veiculo;
+import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface VeiculoRepositorio extends CrudRepository<Veiculo, String> {  }
+@Repository
+public interface VeiculoRepositorio extends CrudRepository<Veiculo, String> {
+
+    @Transactional
+    @Procedure(procedureName = "renavam")
+    String gerarRenavam();
+
+}
